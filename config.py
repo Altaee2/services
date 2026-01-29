76a7735e6dfaff1fd9a4b58d1163cd6542b5f67f
Ali altaee <yl0206382@gmail.com>
2026-01-29 21:52:50 +0300

    Reset

--- config.py
+++
@@ -1,13 +0,0 @@
-TOKEN = "8593409588:AAGyKlVuIl7AlgaB8JatKDSC6GYVxlbjC6I"
-REMOVE_BG_KEY = "PawTQh5RB1AQiqeiW2sS5kpy"
-
-RIGHTS_HEADER = """# 🤍 تلجرام :- @altaee_z
-# 🌐 موقعي : www.ali-Altaee.free.nf
-# ---------------------------------------
-"""
-
-RIGHTS_FOOTER = """
-# ---------------------------------------
-# تم التحويل بواسطة بوت علي الطائي
-# 🤍 تلجرام :- @altaee_z
-"""
